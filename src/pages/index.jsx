14983bcc93e83b42df4ad0e7d39ec492dbc174c0
Silas larsen <silaslarsen80@gmail.com>
2025-05-17 12:15:54 +0200

    d

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,7 +27,7 @@ disse grupper, og det er disse børn, vi gerne vil gøre noget for.</p>
 til at sende dårligt stillede børn på et velfortjent lejrophold på Søgård. Hjælp os med at
 hjælpe dem!</p>
  </div>
-<div><img src="/billeder/fotos/65597_371193986331909_1972008760_n.jpg" alt="hus" /></div>
+<div><img src="public/billeder/fotos/65597_371193986331909_1972008760_n.jpg" alt="hus" /></div>
 </section>
 
 
